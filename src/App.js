82995daad30c8c34cea342e9fcdc96e53e7c8e0f
prashant-gloify <85675989+prashant-gloify@users.noreply.github.com>
2022-02-18 12:49:15 +0530

    Initial setup with routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route, Switch, Link, Routes} from 'react-router-dom';
 import './App.css';
+import Home from './container/Home';
+import About from './container/About';
+import Contact from './container/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div>
+        <Link to="/">Home </Link>
+        <Link to="/about">About Us </Link>
+        <Link to="/contact">contact </Link>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} exact />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route element={Error} />
+      </Routes>
+    </main>
   );
 }
 
---
+++ src/container/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+ 
+function Home (){
+    return (
+        <div className="App">
+            <h1>hello</h1>
+        </div>
+    )
+}
+ 
+export default Home;
